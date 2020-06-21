d47029f249170ba3b316e1ce6249f000591af221
Jacob Daughtry <akutozo@gmail.com>
2020-06-20 20:17:53 -0500

    Added getPlayername function, null name resolved.

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,9 +158,21 @@ var randomNumber = function(min, max) {
     return value;
   };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+      }
+  
+    console.log("Player Robot name is " + name);
+    return name;
+  };
+
 //Player Info
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
